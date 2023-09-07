ce758b4a069d810276ccfcaab9c0e39077f9181d
TRStolsvik <trondstolsvik@gmail.com>
2023-09-07 20:12:51 +0200

    App works.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,32 @@
 (function (app) {
   app.reVerse = function () {
-    //Code here
+    const submitButton = document.getElementById('btnSubmit');
+    submitButton.addEventListener('click', captureString);
   };
+
+  // Capture string from user input
+  function captureString() {
+    // Capture and store user string
+    const userString = document.getElementById('inputString').value;
+
+    const revString = reverseString(userString);
+
+    displayString(revString);
+  }
+
+  // Reverse string
+  function reverseString(userString) {
+    let revString = '';
+
+    for (let i = userString.length - 1; i >= 0; i--) {
+      revString += userString[i];
+    }
+
+    return revString;
+  }
+
+  // Display reversed string on the page
+  function displayString(revString) {
+    document.getElementById('results').innerHTML = revString;
+  }
 })((window.app = window.app || {}));
